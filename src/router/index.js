2ba46a064aaa23c4e7c82bc572188041421f94da
daniel-ciupek <danter005@o2.pl>
2026-02-22 20:03:48 +0100

    Navigation Guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from './routes';
+import { computed } from 'vue';
 
-export default createRouter({
+const router = createRouter({
   routes,
   history: createWebHistory(),
   linkActiveClass: 'active',
 });
+
+router.beforeEach((to, from) => {
+  console.log('Global before each', to, from);
+  if (to.path === '/tasks') return { name: 'login' };
+});
+
+export default router;
