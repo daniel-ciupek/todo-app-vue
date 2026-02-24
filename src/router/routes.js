fb59beabfab5deb71a4e490f64709ceec69878ce
daniel-ciupek <danter005@o2.pl>
2026-02-24 18:59:16 +0100

    Authentication redirection

--- src/router/routes.js
+++ src/router/routes.js
@@ -32,11 +32,17 @@ const routes = [
     path: '/login',
     component: LoginPage,
     name: 'login',
+    meta: {
+      guest: true,
+    },
   },
   {
     path: '/register',
     component: RegisterPage,
     name: 'register',
+    meta: {
+      guest: true,
+    },
   },
 
   {
